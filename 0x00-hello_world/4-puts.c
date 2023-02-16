67811fd9812cfc1b4985ebf5c10544ac7aaf5880
pc <rfiati10@gmail.com>
2023-02-16 15:51:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
