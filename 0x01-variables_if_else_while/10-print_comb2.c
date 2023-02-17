585fb771927ddc206d96172938161484150dd2c7
pc <rfiati10@gmail.com>
2023-02-17 14:34:41 -0800

    10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (!(x == '9' && y == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
