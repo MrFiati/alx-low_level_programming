7ffd0b0bc0cf63b011ed62f1fc1a1f0ca7d9aa2d
pc <rfiati10@gmail.com>
2023-02-17 17:47:16 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int  x, y;
+int nums;
+int nums1;
 
-	for (x = '0'; x <= '9'; x++)
-	{
-		for (y = '0'; y <= '9'; y++)
-		{
-			putchar(x);
-			putchar(y);
-			if (!(x == '9' && y == '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+for (nums = '0'; nums < '9'; nums++)
+{
+for (nums1 = '0'; nums1 <= '9'; nums1++)
+{
+if (nums == '0' && nums1 < '1')
+continue;
+if (nums == '1' && nums1 < '2')
+continue;
+if (nums == '2' && nums1 < '3')
+continue;
+if (nums == '3' && nums1 < '4')
+continue;
+if (nums == '4' && nums1 < '5')
+continue;
+if (nums == '5' && nums1 < '6')
+continue;
+if (nums == '6' && nums1 < '7')
+continue;
+if (nums == '7' && nums1 < '8')
+continue;
+if (nums == '8' && nums1 < '9')
+continue;
+putchar(nums);
+putchar(nums1);
+if (nums == '8' && nums1 == '9')
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
