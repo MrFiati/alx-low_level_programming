44a50e6a1470986cfb7f5e3399514b61e3c9a024
pc <rfiati10@gmail.com>
2023-02-21 08:58:17 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * print_alphabet - prints a string of alphabet
+ *
+ * Return: string of letters a to z
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+}
