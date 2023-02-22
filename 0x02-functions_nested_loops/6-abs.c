8e9b9366754d305b84ea9f830649ece83b8f301a
pc <rfiati10@gmail.com>
2023-02-21 16:45:36 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an int
+ * @n: one night in bangkok and the worlds your oyster
+ *
+ * Return: abs of num
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
