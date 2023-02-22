9661beaecdee17ac138cceaca40197b3fee4d9cc
pc <rfiati10@gmail.com>
2023-02-21 17:13:24 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of jack bauer's day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '2'; a++)
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a == '2' && b == '4')
+				break;
+			for (c = '0'; c <= '5'; c++)
+				for (d = '0'; d <= '9'; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+		}
+}
