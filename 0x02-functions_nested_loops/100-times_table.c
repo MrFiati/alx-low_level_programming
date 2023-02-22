b46492d078beeff4cbf65e50f16fc9a8e9fae173
pc <rfiati10@gmail.com>
2023-02-21 20:16:01 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,43 @@
 #include "main.h"
-/**
- * print_times_table  - give you the multiplication table conditinated by n
- * @n: input that configure the table
- */
-void times_table(int n)
-{
-	int x, y, z, uni, dec, cen;
 
-	if (n <= 15 && n >= 0)
+/**
+ * print_times_table - Prints the times table of the input.
+ * starting with 0
+ * @n: The value of the times table to be printed.
+ */
+
+void print_times_table(int n)
+
+{
+
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
-		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				cen = z / 100;
-				dec = (z / 10);
-				dec = dec % 10;
-				uni = z % 10;
-				if ((y > 0) && (n > 0))
-				{ _putchar(' ');
-					if (!(cen >= 1))
-						_putchar(' ');
-					if (cen >= 1)
-						_putchar('0' + cen);
-					if (!(dec >= 1) && !(cen >= 1))
-						_putchar(' ');
-					if (!(dec >= 1) && (cen >= 1))
-						_putchar('0' + dec);
-					if (dec >= 1)
-						_putchar('0' + dec);
-					_putchar('0' + uni);
-					if (!(y == n))
-						_putchar(','); }
-				else if ((y == 0) && (n > 0))
-				{ _putchar('0' + z);
-					_putchar(','); }
-				else if ((y == 0) && (n == 0))
-					_putchar('0'); }
-			_putchar('\n'); } }
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar((prod / 10) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
 }
