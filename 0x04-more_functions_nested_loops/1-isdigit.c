b732e24420681727f59dd53cd010fc17b88a35bd
pc <rfiati10@gmail.com>
2023-02-23 04:43:59 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - check if is a number between 0 - 9
+ * @c: input
+ * Return: the respective value if is a digit or not
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
