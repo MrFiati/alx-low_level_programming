1efd46f6a5cd471e5abd2cd6718e2f6d76eb14f1
pc <rfiati10@gmail.com>
2023-02-27 17:09:13 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints half a string
+ * @a: array
+ * @n: length of array
+ */
+
+void print_array(int *a, int n)
+{
+	int count;
+
+	count = 0;
+
+	while (count < n)
+	{
+		if (count != (n - 1))
+			printf("%i, ", a[count]);
+		else
+			printf("%i", a[count]);
+		count++;
+	}
+	printf("\n");
+}
