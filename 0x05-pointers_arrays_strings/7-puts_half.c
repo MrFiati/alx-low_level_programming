c9f47cb1f6cd91ef8f3e7045949df95ed12284fa
pc <rfiati10@gmail.com>
2023-02-27 17:00:38 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i, half;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	half = (i) / 2;
+	if (i % 2 != 0)
+		half = ((i - 1) / 2) + 1;
+	for (; half < i; half++)
+		_putchar(str[half]);
+	_putchar('\n');
+}
