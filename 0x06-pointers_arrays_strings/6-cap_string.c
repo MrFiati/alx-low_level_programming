4de492e1f97839c98e7ed310c11043b55fb01668
pc <rfiati10@gmail.com>
2023-03-02 13:55:30 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,32 @@
 #include "main.h"
 
 /**
- * string_toupper - convert to uppercase
- * @a: string in lowercase
- * Return: uppercase
+ * cap_string - take strings and capitalize words
+ * @a: string to capitalize
+ * Return: capitalized words
  */
 
-char *string_toupper(char *a)
+char *cap_string(char *a)
 {
-	int i;
+	int i, x;
+	char *seperators = ",;.!?\"(){} \n\t";
 
 	for (i = 0; *(a + i) != '\0'; i++)
 	{
 		if (*(a + i) >= 'a' && *(a + i) <= 'z')
-			*(a + i) -= 32;
+			for (x = 0; *(seperators + x) != '\0'; x++)
+			{
+				if (*(a + i - 1) == *(seperators + x))
+				{
+					*(a + i) -= 32;
+					break;
+				}
+				else if (i == 0)
+				{
+					*(a + i) -= 32;
+					break;
+				}
+			}
 	}
 	return (a);
 }
