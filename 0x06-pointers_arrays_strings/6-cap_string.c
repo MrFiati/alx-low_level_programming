33373b659e63e8a7131656f4e1f52870638cdf14
pc <rfiati10@gmail.com>
2023-03-02 13:47:33 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert to uppercase
+ * @a: string in lowercase
+ * Return: uppercase
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; *(a + i) != '\0'; i++)
+	{
+		if (*(a + i) >= 'a' && *(a + i) <= 'z')
+			*(a + i) -= 32;
+	}
+	return (a);
+}
