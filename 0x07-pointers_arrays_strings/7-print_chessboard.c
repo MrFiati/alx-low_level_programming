c10b9ce4b07080a1144fd99c5eb2bf2838c23b9a
pc <rfiati10@gmail.com>
2023-03-06 17:17:20 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: 8 by 8 array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, i = 0;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(a[i][x]);
+		if (x == 7 && i < 7)
+		{
+			i++;
+			x = -1;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
