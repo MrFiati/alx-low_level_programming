8fd4b8b79115486932ab01e01ec2961ee288a4ce
MrFiati <rfiati10@gmail.com>
2023-04-03 09:21:50 -0700

    update 0n task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - print the elements of linked list and return # of elements
  * @h: pointer to the first element
- * Return: number of elements
+ * Always Return: number of elements
  */
 
 size_t print_listint(const listint_t *h)
