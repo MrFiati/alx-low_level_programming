564517989f6b25bc6c11ef8e5160ef25994f6399
MrFiati <rfiati10@gmail.com>
2023-06-19 16:56:48 -0700

    4-main.c

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * main - Entry point
+ * Description: Header file that defines a function-like macro
+ * SUM(x, y), that computes the sum of the numbers x and y.
+ * @x: sum of the numbers
+ * @y: the sum of macro
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
