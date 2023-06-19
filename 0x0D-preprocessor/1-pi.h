272d5464a67e38898d0d99835ccb1624f32d3414
MrFiati <rfiati10@gmail.com>
2023-06-19 13:58:49 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * main - Entry point
+ * Description: Header file that defines a macro named PI
+ * as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
