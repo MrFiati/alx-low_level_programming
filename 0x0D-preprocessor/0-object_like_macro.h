f4f7453c56e632e283ba9473700ed621b5074147
MrFiati <rfiati10@gmail.com>
2023-06-19 13:18:53 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: header file that defines a macro named SIZE as
+ * an abbreviation for the token 1024
+ * Return: Always 0 (Success)
+ */
+#define SIZE 1024
+
+#endif
