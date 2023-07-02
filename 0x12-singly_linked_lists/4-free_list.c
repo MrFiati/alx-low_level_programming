55dad2e52613a887fadcc7d3b0eec23cde93e1a8
MrFiati <rfiati10@gmail.com>
2023-07-02 01:28:25 -0700

    4 correction.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,48 +1,21 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_node_end - Entry point
- * Description: Adds new node at the end of a list_t list
- * @head: Pointer to pointer to the head node of the list
- * @str: String stored in the new node
- * Return: Address of the new element, or NULL if it failed
+ * free_list - Entry point
+ * Description: Frees a linked list
+ * @head: A pointer to the head of the list
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-	list_t *new, *current;
+	list_t *current;
 
-	if (!head)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
-	/* Duplicate the string */
-	new->str = strdup(str);
-	if (!new->str)
+	while (head)
 	{
-		free(new);
-		return (NULL);
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
 	}
-	/* Count the number of characters within square brackets */
-	new->len = strlen(str);
-	new->next = NULL;
-	/* If the list is empty, the new node becomes the head */
-	if (!*head)
-	{
-		*head = new;
-		return (new);
-	}
-	current = *head;
-	/* Traverse the list until the last node is reached */
-	while (current->next)
-	{
-		current = current->next;
-	}
-	/* Add the new node to the end of the list */
-	current->next = new;
-	return (new);
 }
