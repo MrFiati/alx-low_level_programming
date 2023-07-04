4cf2c3dc19fa13ea964f92465c15f42cdabf7839
MrFiati <rfiati10@gmail.com>
2023-07-04 15:27:17 -0700

    101-print_listint_safe with executable files

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * reverse_listint - Entry Point...
+ * Description: Reverses a listint_t linked list...
+ * @head: A pointer to the address of
+ * the head of the list_t list.
+ *
+ * Return: A pointer to the first node of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ahead, *behind;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	behind = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		ahead = (*head)->next;
+		(*head)->next = behind;
+		behind = *head;
+		*head = ahead;
+	}
+
+	(*head)->next = behind;
+
+	return (*head);
+}
