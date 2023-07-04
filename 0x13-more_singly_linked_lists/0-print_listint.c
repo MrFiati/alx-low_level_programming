d0bad8c93531068d167c8a3ccdc3e5c6c47cf648
MrFiati <rfiati10@gmail.com>
2023-07-04 09:21:32 -0700

    putchar,main,lists.h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_listint - Entry point...
+ *
+ * Description: Function that prints all the elements of a listint_t list
+ * @h: pointer to the listint_t
+ *
+ * Return: Number of nodes in list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * listint_len - Entry point...
+ * Description: Returns number of elements in linked listint_t list
+ * @h: Pointer to the head of the linked list
+ * Return: Number of nodes in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count_nodes = 0;
+
+	while (h != 0)
+	{
+		/* Increment count for each node */
+		count_nodes++;
+		/* Next node */
+		h = h->next;
+	}
+	return (count_nodes);
+}
